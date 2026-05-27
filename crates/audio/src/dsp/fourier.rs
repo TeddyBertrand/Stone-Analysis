c32ee51db0ab8a3be41ae42ebc56b70eceedb90a
Frederick Jeffcock <frederick.jeffcock@epitech.eu>
2026-05-27 16:21:35 +0200

    refactor(Audio): add idft calc

--- crates/audio/src/dsp/fourier.rs
+++ crates/audio/src/dsp/fourier.rs
@@ -1,4 +1,4 @@
-use std::f32::consts::PI;
+use std::{f32::consts::PI, vec};
 
 #[derive(Debug, Clone, Copy)]
 pub struct Complex {
@@ -13,6 +13,26 @@ impl Complex {
     pub fn magnitude(&self) -> f32 {
         (self.re * self.re + self.im * self.im).sqrt()
     }
+}
+
+pub fn idft(freq_samples: &Vec<Complex>) -> Vec<f32> 
+{
+    let n = freq_samples.len();
+    let mut reconstructed_freq: Vec<f32> = vec![0.0; n];
+
+    for k in 0..n {
+        let mut sum_real = 0.0;
+
+        for n_idx in 0..n {
+            let angle = (2.0 * PI * (k as f32) * (n_idx as f32)) / (n as f32);
+
+            sum_real += (freq_samples[n_idx].re * angle.cos()) - (freq_samples[n_idx].im * angle.sin());
+        }
+
+        reconstructed_freq[k] = sum_real / (n as f32);
+    }
+
+    return reconstructed_freq;
 }
 
 pub fn dft(samples: &[f32]) -> Vec<Complex> {
